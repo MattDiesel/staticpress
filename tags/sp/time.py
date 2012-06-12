faf15a67df26baf5c7ad65b31fe45d62a49a09cd
Matt Diesel <M@ttDiesel.co.uk>
2012-06-12 16:30:50 +0100

    Cleared project files

--- tags/sp/time.py
+++
@@ -1,8 +0,0 @@
-
-from datetime import datetime
-
-def handler(attrs):
-	if (attrs != None) and ('format' in attrs.keys()):
-		return datetime.now().strftime(attrs['format'])
-	else:
-		return datetime.now().ctime()
